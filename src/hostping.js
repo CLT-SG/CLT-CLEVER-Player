bbc8841e7ca2c498e9b61cb36e47387b9473c52b
fahmi <fahmi@closed-loop.biz>
2023-11-03 14:48:13 +0800

    haha

--- src/hostping.js
+++ src/hostping.js
@@ -1,4 +1,4 @@
-const ping = require('ping') //your life too sick and pls go hospital to check your mentalhealth
+const ping = require('ping') //your life too sick and pls go hospital to check your mental health
 const config = require('C:/app/config')
 
 module.exports.checkhost = function() {
