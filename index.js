05929094911bb1a20db9427a64000d45b0931277
fahmi <fahmi@closed-loop.biz>
2024-01-25 12:00:08 +0800

    add redirect to login

--- index.js
+++ index.js
@@ -131,8 +131,8 @@ try {
     app.on('ready', () => {
       mainWin = new BrowserWindow({
         backgroundColor: '#302d2d',
-        //alwaysOnTop: true,
-        //autoHideMenuBar: true,
+        alwaysOnTop: true,
+        autoHideMenuBar: true,
         fullscreenable: false,
         resizable: false,
         moveable: false,
@@ -160,8 +160,8 @@ try {
         width: 0,
         height: 0,
         backgroundColor: '#302d2d',
-        //alwaysOnTop: true,
-        //autoHideMenuBar: true,
+        alwaysOnTop: true,
+        autoHideMenuBar: true,
         fullscreenable: false,
         resizable: false,
         moveable: false,
@@ -218,14 +218,14 @@ try {
       })
 
       //hide menu bar
-      //mainWin.setSkipTaskbar(true)
-      //mainWin.setAlwaysOnTop(true)
-      //mainWin.setMenuBarVisibility(false)
+      mainWin.setSkipTaskbar(true)
+      mainWin.setAlwaysOnTop(true)
+      mainWin.setMenuBarVisibility(false)
       Menu.setApplicationMenu(null)
       mainWin.setMenu(null)
-      //serverWin.setSkipTaskbar(true)
-      //serverWin.setAlwaysOnTop(true)
-      //serverWin.setMenuBarVisibility(false)
+      serverWin.setSkipTaskbar(true)
+      serverWin.setAlwaysOnTop(true)
+      serverWin.setMenuBarVisibility(false)
       Menu.setApplicationMenu(null)
       serverWin.setMenu(null)
 
