705d8abdcb58a212613e8eeacaf78a1fc410e691
fahmi <fahmi@closed-loop.biz>
2024-09-19 16:03:15 +0800

    revert to original working version

--- create-config.js
+++ create-config.js
@@ -202,45 +202,42 @@ class CreateConfig {
             console.error('Failed to load configuration. Check the config file.');
             return;
         }
-        await isReachable('http://' + config.controller, {
-            timeout: 10000
-        }).then((status) => {
-            macaddress.one(function (err, mac) {
-                const secret = 'Clt@2022';
-                const hash = Crypto.createHash('sha256', secret).update(mac).digest('hex');
-                if (config.serialkey == hash) {
-                    if (status == true) {
-                        if (mainWin) {
-                            if (config.ctrltype == 'videowall') mainWin.loadURL('http://' + config.controller + '/preview/' + config.tempid + '/videowall/false')
-                            if (config.ctrltype == 'console' && serverWin) {
-                                mainWin.loadURL('http://' + config.controller + '/preview/login')
-                                serverWin.show()
-                                serverWin.setBounds({
-                                    x: 0,
-                                    y: 0,
-                                    width: screen.getPrimaryDisplay().workArea.width,
-                                    height: screen.getPrimaryDisplay().workArea.height
-                                })
-                                serverWin.loadURL('http://' + config.controller)
-                                serverWin.blur()
-                            }
-                            mainWin.setBounds({
+        var status = true
+        macaddress.one(function (err, mac) {
+            const secret = 'Clt@2022';
+            const hash = Crypto.createHash('sha256', secret).update(mac).digest('hex');
+            if (config.serialkey == hash) {
+                if (status == true) {
+                    if (mainWin) {
+                        if (config.ctrltype == 'videowall') mainWin.loadURL('http://' + config.controller + '/preview/' + config.tempid + '/videowall/false')
+                        if (config.ctrltype == 'console' && serverWin) {
+                            mainWin.loadURL('http://' + config.controller + '/preview/login')
+                            serverWin.show()
+                            serverWin.setBounds({
                                 x: 0,
                                 y: 0,
                                 width: screen.getPrimaryDisplay().workArea.width,
                                 height: screen.getPrimaryDisplay().workArea.height
                             })
-                            mainWin.focus()
+                            serverWin.loadURL('http://' + config.controller)
+                            serverWin.blur()
                         }
-                        log.info('Server http://' + config.controller + '/preview/' + config.tempid + ' is online')
-                    } else {
-                        mainWin.loadURL("file://" + __dirname + "/src/offline.html")
-                        log.warn('Server http://' + config.cleverweb + ' is offline')
+                        mainWin.setBounds({
+                            x: 0,
+                            y: 0,
+                            width: screen.getPrimaryDisplay().workArea.width,
+                            height: screen.getPrimaryDisplay().workArea.height
+                        })
+                        mainWin.focus()
                     }
+                    log.info('Server http://' + config.controller + '/preview/' + config.tempid + ' is online')
                 } else {
-                    mainWin.loadURL("file://" + __dirname + "/src/activate.html")
+                    mainWin.loadURL("file://" + __dirname + "/src/offline.html")
+                    log.warn('Server http://' + config.cleverweb + ' is offline')
                 }
-            });
+            } else {
+                mainWin.loadURL("file://" + __dirname + "/src/activate.html")
+            }
         });
     }
 }
